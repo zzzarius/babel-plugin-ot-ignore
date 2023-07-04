ab86bb2f52d7459eb55ca54542348eda02b093f8
Darius Zivertas <d.zivertas@gmail.com>
2023-07-04 13:40:56 +0300

    Lint

--- __tests__/babel-plugin-ot-ignore.test.js
+++ __tests__/babel-plugin-ot-ignore.test.js
@@ -1,18 +1,18 @@
-const { transform: t } = require('@babel/core');
-const otIgnore = require('../babel-plugin-ot-ignore');
+const { transform: t } = require('@babel/core')
+const otIgnore = require('../babel-plugin-ot-ignore')
 
 const plainTransform = (code) =>
   t(code, {
     presets: ['@babel/preset-react'],
-    configFile: false,
-  }).code.trim();
+    configFile: false
+  }).code.trim()
 describe('babel-plugin-ot-ignore defaults', () => {
   const transform = (code) =>
     t(code, {
       plugins: [otIgnore],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
 
   test('Check if data attribute data-ot-ignore="" and class being added', () => {
     const input = `
@@ -21,7 +21,7 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -29,10 +29,10 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" data-ot-ignore="" className="optanon-category-C0001" />
         </div>
       );
-    `;
+    `
 
-    expect(transform(input)).toBe(plainTransform(expectedOutput));
-  });
+    expect(transform(input)).toBe(plainTransform(expectedOutput))
+  })
 
   test('Check if className is appended to existing classes', () => {
     const input = `
@@ -41,7 +41,7 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="existing-class" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -49,10 +49,10 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="existing-class optanon-category-C0001" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
-    expect(transform(input)).toBe(plainTransform(expectedOutput));
-  });
+    expect(transform(input)).toBe(plainTransform(expectedOutput))
+  })
 
   test('Check if className is appended to existing classes even with empty string', () => {
     const input = `
@@ -61,7 +61,7 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -69,10 +69,10 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="optanon-category-C0001" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
-    expect(transform(input)).toBe(plainTransform(expectedOutput));
-  });
+    expect(transform(input)).toBe(plainTransform(expectedOutput))
+  })
 
   test('Check if className is appended to existing classes even if it`s empty', () => {
     const input = `
@@ -81,7 +81,7 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -89,10 +89,10 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="optanon-category-C0001" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
-    expect(transform(input)).toBe(plainTransform(expectedOutput));
-  });
+    expect(transform(input)).toBe(plainTransform(expectedOutput))
+  })
 
   test('Check if className expressions are working', () => {
     const input = `
@@ -101,7 +101,7 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="optanon-category-C0001" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -109,11 +109,11 @@ describe('babel-plugin-ot-ignore defaults', () => {
           <img src="http://placekitten.com/200/300" className="optanon-category-C0001" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
-    expect(transform(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transform(input)).toBe(plainTransform(expectedOutput))
+  })
+})
 
 describe('babel-plugin-ot-ignore srcValueRegex', () => {
   const transformRegex = (code) =>
@@ -122,13 +122,13 @@ describe('babel-plugin-ot-ignore srcValueRegex', () => {
         [
           otIgnore,
           {
-            srcValueRegex: /^http:\/\/placekitten.com/,
-          },
-        ],
+            srcValueRegex: /^http:\/\/placekitten.com/
+          }
+        ]
       ],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
   test('Should add ot attributes if regex matches', () => {
     const input = `
       const MyComponent = () => (
@@ -137,7 +137,7 @@ describe('babel-plugin-ot-ignore srcValueRegex', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -146,11 +146,11 @@ describe('babel-plugin-ot-ignore srcValueRegex', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
-    expect(transformRegex(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transformRegex(input)).toBe(plainTransform(expectedOutput))
+  })
+})
 
 describe('babel-plugin-ot-ignore srcValueRegex as string', () => {
   const transformRegex = (code) =>
@@ -159,13 +159,13 @@ describe('babel-plugin-ot-ignore srcValueRegex as string', () => {
         [
           otIgnore,
           {
-            srcValueRegex: '^http://placekitten.com',
-          },
-        ],
+            srcValueRegex: '^http://placekitten.com'
+          }
+        ]
       ],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
   test('Should add ot attributes if regex matches', () => {
     const input = `
       const MyComponent = () => (
@@ -174,7 +174,7 @@ describe('babel-plugin-ot-ignore srcValueRegex as string', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -183,11 +183,11 @@ describe('babel-plugin-ot-ignore srcValueRegex as string', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
-    expect(transformRegex(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transformRegex(input)).toBe(plainTransform(expectedOutput))
+  })
+})
 
 describe('babel-plugin-ot-ignore otDataAttribute', () => {
   const transformDataAttribute = (code) =>
@@ -196,13 +196,13 @@ describe('babel-plugin-ot-ignore otDataAttribute', () => {
         [
           otIgnore,
           {
-            otDataAttribute: 'data-my-custom-attribute',
-          },
-        ],
+            otDataAttribute: 'data-my-custom-attribute'
+          }
+        ]
       ],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
   test('Custom otDataAttribute', () => {
     const input = `
       const MyComponent = () => (
@@ -211,7 +211,7 @@ describe('babel-plugin-ot-ignore otDataAttribute', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -220,11 +220,11 @@ describe('babel-plugin-ot-ignore otDataAttribute', () => {
           <img src="http://example.com" data-my-custom-attribute="" className="optanon-category-C0001" />
         </div>
       );
-    `;
+    `
 
-    expect(transformDataAttribute(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transformDataAttribute(input)).toBe(plainTransform(expectedOutput))
+  })
+})
 
 describe('babel-plugin-ot-ignore otClassName', () => {
   const transformClassName = (code) =>
@@ -233,13 +233,13 @@ describe('babel-plugin-ot-ignore otClassName', () => {
         [
           otIgnore,
           {
-            otClassName: 'my-custom-class',
-          },
-        ],
+            otClassName: 'my-custom-class'
+          }
+        ]
       ],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
   test('Custom class name', () => {
     const input = `
       const MyComponent = () => (
@@ -248,7 +248,7 @@ describe('babel-plugin-ot-ignore otClassName', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -257,11 +257,11 @@ describe('babel-plugin-ot-ignore otClassName', () => {
           <img src="http://example.com" data-ot-ignore="" className="my-custom-class" />
         </div>
       );
-    `;
+    `
 
-    expect(transformClassName(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transformClassName(input)).toBe(plainTransform(expectedOutput))
+  })
+})
 
 describe('babel-plugin-ot-ignore opt out adding class', () => {
   const transformClassName = (code) =>
@@ -270,13 +270,13 @@ describe('babel-plugin-ot-ignore opt out adding class', () => {
         [
           otIgnore,
           {
-            otClassName: false,
-          },
-        ],
+            otClassName: false
+          }
+        ]
       ],
       presets: ['@babel/preset-react'],
-      configFile: false,
-    }).code.trim();
+      configFile: false
+    }).code.trim()
   test('Should not add class if it`s disabled', () => {
     const input = `
       const MyComponent = () => (
@@ -285,7 +285,7 @@ describe('babel-plugin-ot-ignore opt out adding class', () => {
           <img src="http://example.com" />
         </div>
       );
-    `;
+    `
 
     const expectedOutput = `
       const MyComponent = () => (
@@ -294,8 +294,8 @@ describe('babel-plugin-ot-ignore opt out adding class', () => {
           <img src="http://example.com" data-ot-ignore="" />
         </div>
       );
-    `;
+    `
 
-    expect(transformClassName(input)).toBe(plainTransform(expectedOutput));
-  });
-});
+    expect(transformClassName(input)).toBe(plainTransform(expectedOutput))
+  })
+})
--- babel-plugin-ot-ignore.js
+++ babel-plugin-ot-ignore.js
@@ -1,103 +1,103 @@
-const { declare } = require('@babel/helper-plugin-utils');
+const { declare } = require('@babel/helper-plugin-utils')
 
 module.exports = declare((api, options) => {
-  api.assertVersion(7);
-  const { types: t } = api;
+  api.assertVersion(7)
+  const { types: t } = api
 
   return {
     name: 'babel-plugin-ot-ignore',
 
     visitor: {
-      JSXElement(path, state) {
+      JSXElement (path, state) {
         const {
           otDataAttribute = 'data-ot-ignore',
           otClassName = 'optanon-category-C0001',
-          srcValueRegex = false,
-        } = state.opts;
-        const { openingElement } = path.node;
+          srcValueRegex = false
+        } = state.opts
+        const { openingElement } = path.node
 
-        function testSrcValueRegex(value) {
+        function testSrcValueRegex (value) {
           if (!srcValueRegex) {
-            return true;
+            return true
           }
-          const regexp = new RegExp(srcValueRegex);
-          return regexp.test(value);
+          const regexp = new RegExp(srcValueRegex)
+          return regexp.test(value)
         }
 
         const hasNoSrc = !openingElement.attributes?.some(
           (attr) =>
             attr.name?.name === 'src' && testSrcValueRegex(attr.value?.value)
-        );
+        )
         if (hasNoSrc) {
-          return;
+          return
         }
 
         const alreadyHasOt = openingElement.attributes?.some(
           (attr) => attr.name?.name === otDataAttribute
-        );
+        )
 
         if (otDataAttribute && !alreadyHasOt) {
           const additionalAttribute = t.jsxAttribute(
             t.jsxIdentifier(otDataAttribute),
             t.stringLiteral('')
-          );
-          openingElement.attributes.push(additionalAttribute);
+          )
+          openingElement.attributes.push(additionalAttribute)
         }
 
         if (!otClassName) {
-          return;
+          return
         }
 
         const hasNoOtherClasses = !openingElement.attributes?.some(
           (attr) => attr.name?.name === 'className'
-        );
+        )
 
         if (hasNoOtherClasses) {
           const additionalAttribute = t.jsxAttribute(
             t.jsxIdentifier('className'),
             t.stringLiteral(otClassName)
-          );
-          openingElement.attributes?.push(additionalAttribute);
-          return;
+          )
+          openingElement.attributes?.push(additionalAttribute)
+          return
         }
 
         const existingClassAttribute = openingElement.attributes.find(
           (attr) => attr.name?.name === 'className'
-        );
+        )
 
         // <img src="..." className />
         if (!existingClassAttribute.value) {
-          existingClassAttribute.value = t.stringLiteral(otClassName);
-          return;
+          existingClassAttribute.value = t.stringLiteral(otClassName)
+          return
         }
 
         if (t.isJSXExpressionContainer(existingClassAttribute.value)) {
-          const quasis = existingClassAttribute.value?.expression?.quasis;
+          const quasis = existingClassAttribute.value?.expression?.quasis
           if (!Array.isArray(quasis)) {
-            existingClassAttribute.value = t.stringLiteral(otClassName);
-            return;
+            existingClassAttribute.value = t.stringLiteral(otClassName)
+            return
           }
           quasis.at(0).value.raw = [otClassName, quasis.at(0).value.raw].join(
             ' '
-          );
+          )
           quasis.at(0).value.cooked = [
             otClassName,
-            quasis.at(0).value.cooked,
-          ].join(' ');
-          return;
+            quasis.at(0).value.cooked
+          ].join(' ')
+          return
         }
 
-        const existingClassAttributeValue = existingClassAttribute.value?.value;
+        const existingClassAttributeValue = existingClassAttribute.value?.value
         const hasOptanonClass =
-          existingClassAttributeValue?.includes(otClassName);
+          existingClassAttributeValue?.includes(otClassName)
         if (hasOptanonClass) {
-          return;
+          return
         }
 
-        const classes = existingClassAttributeValue.split(' ');
-        classes.push(otClassName);
-        existingClassAttribute.value.value = classes.join(' ').trim();
-      },
-    },
-  };
-});
+        const classes = existingClassAttributeValue.split(' ')
+        classes.push(otClassName)
+        existingClassAttribute.value.value = classes.join(' ').trim()
+      }
+    }
+  }
+})
